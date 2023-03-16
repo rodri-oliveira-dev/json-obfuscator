7f9503383be1626c0249ca371dbe8eba94fc1aa1
Rodrigo de Oliveira <rodrigodotnet@outlook.com>
2023-03-16 14:02:03 -0300

    Update teste10.kt

--- app/src/teste10.kt
+++ app/src/teste10.kt
@@ -1,7 +1,58 @@
 #!/bin/bash
 
-# Verifica se há commits não enviados ou arquivos modificados, adicionados ou excluídos na branch atual
-if [[ -n $(git log --name-status --diff-filter=ADM origin/$(git rev-parse --abbrev-ref HEAD)..HEAD) ]]; then
-    # Se houver, executa o comando gradlew clean build
-    ./gradlew clean build
+# Verifica se existem commits que não foram enviados
+if git log origin/main..HEAD &> /dev/null; then
+    echo "Existem commits que ainda não foram enviados para o repositório remoto."
+    # Verifica se existem arquivos modificados, novos ou excluídos
+    if git status --porcelain | grep -E '^(M|A|D)' &> /dev/null; then
+        echo "Existem arquivos modificados, novos ou excluídos que precisam ser adicionados e commitados."
+        # Executa o comando "gradlew build"
+        ./gradlew build
+    fi
 fi
+
+
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.FilterMatcher
+import io.micronaut.http.filter.HttpFilter
+import io.micronaut.http.filter.ServerFilterChain
+import org.reactivestreams.Publisher
+
+class RequestDataFilter : HttpFilter {
+    override fun doFilter(request: HttpRequest<*>, chain: ServerFilterChain): Publisher<out HttpResponse<*>> {
+        val requestId = request.headers.get("X-Request-ID") ?: generateRequestId()
+        val requestOf = request.headers.get("X-Request-Of") ?: "Unknown"
+
+        RequestContextHolder.setRequestData(RequestData(requestId, requestOf))
+
+        return chain.proceed(request).doFinally {
+            RequestContextHolder.clear()
+        }
+    }
+
+    private fun generateRequestId(): String {
+        // Implemente sua lógica de geração de ID de requisição aqui
+        return UUID.randomUUID().toString()
+    }
+}
+
+
+
+
+object RequestContextHolder {
+    private val contextHolder = ThreadLocal<RequestData>()
+
+    fun setRequestData(requestData: RequestData) {
+        contextHolder.set(requestData)
+    }
+
+    fun getRequestData(): RequestData? {
+        return contextHolder.get()
+    }
+
+    fun clear() {
+        contextHolder.remove()
+    }
+}
