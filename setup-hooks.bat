18433227f536b720d20d701c879dbec52989ef1d
Rodrigo de Oliveira <rodrigodotnet@outlook.com>
2025-01-03 14:11:52 -0300

    Atualizando dependÃªncias

--- setup-hooks.bat
+++ setup-hooks.bat
@@ -31,3 +31,4 @@ echo Hooks copiados para .git/hooks.
 /* Modificação única: Refatorando função Y em 2024-12-06T12:41:25-03:00 - 923174457 */
 /* Modificação única: Aprimorando performance em 2024-12-09T10:23:56-03:00 - 632930023 */
 /* Modificação única: Atualizando dependências em 2025-01-03T18:05:40-03:00 - 732779705 */
+/* Modificação única: Atualizando dependências em 2025-01-03T14:11:52-03:00 - 2011592077 */
