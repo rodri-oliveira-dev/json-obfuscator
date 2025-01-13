eb4c5dbaf7d3adcb3324ec8bbfbe45a18a19e33e
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2025-01-13 14:30:14 -0300

    Atualizar o teste.cs

--- tmp/teste.cs
+++ tmp/teste.cs
@@ -2,22 +2,28 @@ public static IApplicationBuilder IncludeSecurityHeaders(this IApplicationBuilde
 {
     app.Use(async (context, next) =>
     {
-        context.Response.Headers.Add("X-Frame-Options", "DENY");
-        context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-        context.Response.Headers.Add("Content-Security-Policy", "default-src 'none'; " +
-                                                                "script-src 'self' 'strict-dynamic'; " +
-                                                                "font-src 'self' https://fonts.gstatic.com; " +
-                                                                "img-src 'self' data:; " +
-                                                                "connect-src 'self'; " +
-                                                                "style-src 'self' https://fonts.googleapis.com");
-        context.Response.Headers.Add("Permissions-Policy", "camera=(), microphone=(), geolocation=(), accelerometer=()");
-        context.Response.Headers.Add("Referrer-Policy", "no-referrer");
-        context.Response.Headers.Add("Cross-Origin-Resource-Policy", "same-origin");
-        context.Response.Headers.Add("Origin-Agent-Cluster", "?1");
-        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
-        context.Response.Headers.Add("Expect-CT", "max-age=86400, enforce");
-        context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
-        context.Response.Headers.Remove("Server"); // Oculta o servidor
+        var headers = context.Response.Headers;
+
+        headers.Append("X-Frame-Options", "DENY");
+        headers.Append("X-Content-Type-Options", "nosniff");
+        headers.Append("Content-Security-Policy", "default-src 'none'; " +
+                                                   "script-src 'self' 'strict-dynamic'; " +
+                                                   "font-src 'self' https://fonts.gstatic.com; " +
+                                                   "img-src 'self' data:; " +
+                                                   "connect-src 'self'; " +
+                                                   "style-src 'self' https://fonts.googleapis.com");
+        headers.Append("Permissions-Policy", "camera=(), microphone=(), geolocation=(), accelerometer=()");
+        headers.Append("Referrer-Policy", "no-referrer");
+        headers.Append("Cross-Origin-Resource-Policy", "same-origin");
+        headers.Append("Origin-Agent-Cluster", "?1");
+        headers.Append("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+        headers.Append("Expect-CT", "max-age=86400, enforce");
+        headers.Append("X-Permitted-Cross-Domain-Policies", "none");
+
+        if (headers.ContainsKey("Server"))
+        {
+            headers.Remove("Server"); // Oculta o servidor
+        }
 
         await next();
     });
