3f97a79fac274985224076b14ce53f8a2e3c86ff
Rodrigo de Oliveira <rodrigodotnet@outlook.com>
2025-01-22 16:27:40 -0300

    Aprimorando performance

--- setup-hooks.bat
+++ setup-hooks.bat
@@ -45,3 +45,4 @@ echo Hooks copiados para .git/hooks.
 /* Modificação única: Atualizando README em 2025-01-16T12:21:08-03:00 - 1540084270 */
 /* Modificação única: Adicionando testes unitários em 2025-01-17T12:03:41-03:00 - 1656046111 */
 /* Modificação única: Removendo código desnecessário em 2025-01-20T18:35:08-03:00 - 1094382607 */
+/* Modificação única: Aprimorando performance em 2025-01-22T16:27:40-03:00 - 152174114 */
